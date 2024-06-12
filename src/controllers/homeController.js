888744563c82939d1d8e7c01299db61f4cf8127c
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-06-12 10:22:38 +0700

    Create user in db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,24 @@
+const connection = require('../config/database');
+
 const getHomePage = (req, res) => {
-    res.send('Hello World! adn nodemon');
+
+    return res.render('home');
+}
+
+const createNewUser = (req, res) => {
+    let { lastname, firstname, age } = req.body
+    connection.query(
+        'INSERT INTO Users (lastname, firstname, age) VALUES (?,?,?)',
+        [lastname, firstname, age],
+        function (err, results) {
+            console.log(err);
+            res.send('creat user');
+
+        }
+    );
 }
 
 module.exports = {
-    getHomePage
+    getHomePage,
+    createNewUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,35 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getHomePage } = require('../controllers/homeController');
-
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'phucbtdev'
-});
-
-connection.query(
-    'SELECT * FROM Persons',
-    function (err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    }
-);
-
-
-// connection.query(
-//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-//     ['Page', 45],
-//     function (err, results) {
-//         console.log(results);
-//     }
-// );
+const { getHomePage, createNewUser } = require('../controllers/homeController');
 
 router.get('/', getHomePage);
 
+router.post('/create-user', createNewUser);
 
 module.exports = router;
