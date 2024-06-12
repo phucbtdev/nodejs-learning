58a1876b232bdcceb9857b617a1f371b1e4f434c
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-06-12 10:22:22 +0700

    Get data form

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST_NAME;
 
+//config
+app.use(express.json());
+app.use(express.urlencoded());
+
 //Config ViewEngine
 configViewEngine(app);
 
